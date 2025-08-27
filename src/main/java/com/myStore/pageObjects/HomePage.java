89a725c96cce09d4ddf80b43bd94d1a039acd368
afn-amd <ahmedaffan146@gmail.com>
2025-08-27 15:46:00 +0530

    Home Page added

--- src/main/java/com/myStore/pageObjects/HomePage.java
+++ src/main/java/com/myStore/pageObjects/HomePage.java
@@ -1,5 +1,30 @@
 package com.myStore.pageObjects;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+
+public class HomePage extends BaseClass {
+	
+	//@FindBy(xpath="")
+	//WebElement myWishList;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/div/div/ul/li[1]/a/span")
+	WebElement orderHistory;
+	
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	/*public boolean validateMyWishList() throws Throwable{
+		return Action.isDisplayed(driver, myWishList);
+	}*/
+	
+	public void validateOrderHistory() throws Throwable {
+		Action.isDisplayed(driver, orderHistory);
+	}
 
 }
