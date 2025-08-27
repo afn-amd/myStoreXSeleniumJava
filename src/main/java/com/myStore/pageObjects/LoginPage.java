4b352055e8f1ea5406a2aa796cb819e9f5c8b704
afn-amd <ahmedaffan146@gmail.com>
2025-08-27 15:12:49 +0530

    Login Page added

--- src/main/java/com/myStore/pageObjects/LoginPage.java
+++ src/main/java/com/myStore/pageObjects/LoginPage.java
@@ -1,5 +1,43 @@
 package com.myStore.pageObjects;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+
+public class LoginPage extends BaseClass {
+	
+	@FindBy(id="email")
+	WebElement username;
+	
+	@FindBy(id="passwd")
+	WebElement password;
+	
+	@FindBy(id="SubmitLogin")
+	WebElement signInButton;
+	
+	@FindBy(name="email_create")
+	WebElement emailForNewAccount;
+	
+	@FindBy(name="SubmitCreate")
+	WebElement createNewAccountButton;
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public HomePage login(String uname, String pswd) throws Throwable {
+		Action.type(username, uname);
+		Action.type(password, pswd);
+		Action.click(driver, signInButton);
+		return new HomePage();
+	}
+	
+	public AccountCreationPage createNewAccount(String newEmail) throws Throwable {
+		Action.type(emailForNewAccount, newEmail);
+		return new AccountCreationPage();
+	}
 
 }
