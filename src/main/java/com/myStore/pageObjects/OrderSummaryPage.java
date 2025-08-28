3ae7746ce1c70c5928ea18964ecfdba57fac69ae
afn-amd <ahmedaffan146@gmail.com>
2025-08-28 20:09:45 +0530

    OrderSummaryPage added

--- src/main/java/com/myStore/pageObjects/OrderSummaryPage.java
+++ src/main/java/com/myStore/pageObjects/OrderSummaryPage.java
@@ -1,5 +1,24 @@
 package com.myStore.pageObjects;
 
-public class OrderSummaryPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+
+public class OrderSummaryPage extends BaseClass {
+
+	@FindBy(xpath = "//span[contains(text(),'I confirm my order')]")
+	WebElement confirmOrderButton;
+
+	public OrderSummaryPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	public OrderConfirmationPage clickOnConfirmOrderButton() {
+		Action.click(driver, confirmOrderButton);
+		return new OrderConfirmationPage();
+	}
 
 }
