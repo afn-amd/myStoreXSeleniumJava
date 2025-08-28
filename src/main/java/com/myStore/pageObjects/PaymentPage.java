b645a255a276349df1aaea90ce64b78bf23e631b
afn-amd <ahmedaffan146@gmail.com>
2025-08-28 19:57:15 +0530

    PaymentPage added

--- src/main/java/com/myStore/pageObjects/PaymentPage.java
+++ src/main/java/com/myStore/pageObjects/PaymentPage.java
@@ -1,5 +1,27 @@
 package com.myStore.pageObjects;
 
-public class PaymentPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+
+public class PaymentPage extends BaseClass {
+	
+	@FindBy(xpath="//a[contains(text(),'Pay by bank wire')]")
+	WebElement bankWireMethod;
+	
+	@FindBy(xpath="//a[contains(text(),'Pay by check')]")
+	WebElement payByCheckMethod;
+	
+	public PaymentPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public OrderSummaryPage clickOnPaymentMethod() throws Throwable {
+		Action.click(driver, bankWireMethod);
+		return new OrderSummaryPage();
+	}
 
 }
