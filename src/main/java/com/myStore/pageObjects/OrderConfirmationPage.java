04d30182b7d37f3358b6c4d574191ff1a26bb209
afn-amd <ahmedaffan146@gmail.com>
2025-08-28 21:34:43 +0530

    all page classes added

--- src/main/java/com/myStore/pageObjects/OrderConfirmationPage.java
+++ src/main/java/com/myStore/pageObjects/OrderConfirmationPage.java
@@ -1,5 +1,23 @@
 package com.myStore.pageObjects;
 
-public class OrderConfirmationPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.myStore.base.BaseClass;
+
+public class OrderConfirmationPage extends BaseClass {
+	
+	@FindBy(xpath="//p/strong[contains(text(),'Your order on My Store is complete.')]")
+	WebElement confirmMessage;
+	
+	public OrderConfirmationPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String validateConfirmMessage() {
+		String confirmMsg = confirmMessage.getText();
+		return confirmMsg;
+	}
 
 }
