d5f24d96264d9bdad2a2ed88ad374c116a113dea
afn-amd <ahmedaffan146@gmail.com>
2025-08-28 19:30:48 +0530

    ShippigPage added

--- src/main/java/com/myStore/pageObjects/ShippingPage.java
+++ src/main/java/com/myStore/pageObjects/ShippingPage.java
@@ -1,5 +1,31 @@
 package com.myStore.pageObjects;
 
-public class ShippingPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+
+public class ShippingPage extends BaseClass {
+	
+	@FindBy(id="cgv")
+	WebElement terms;
+	
+	@FindBy(xpath="//button/span[contains(text(),'Proceed to checkout')]")
+	WebElement proceedToCheckOutButton;
+	
+	public ShippingPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void checkTheTerms() throws Throwable {
+		Action.click(driver, terms);
+	}
+	
+	public PaymentPage clickOnProceedToCheckOut() throws Throwable {
+		Action.click(driver, proceedToCheckOutButton);
+		return new PaymentPage();
+	}
 
 }
