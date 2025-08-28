1ea97defee958492c920431790d040eed9586c86
afn-amd <ahmedaffan146@gmail.com>
2025-08-28 12:28:24 +0530

    AddressPage added

--- src/main/java/com/myStore/pageObjects/AddressPage.java
+++ src/main/java/com/myStore/pageObjects/AddressPage.java
@@ -1,5 +1,24 @@
 package com.myStore.pageObjects;
 
-public class AddressPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+
+public class AddressPage extends BaseClass {
+	
+	@FindBy(xpath="//span[text()='Proceed to checkout']")
+	WebElement proceedToCheckOut;
+	
+	public AddressPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public ShippingPage clickOnCheckOut() throws Throwable {
+		Action.click(driver, proceedToCheckOut);
+		return new ShippingPage();
+	}
 
 }
