aeabe3a820e7c715368503740216c0aa30432073
afn-amd <ahmedaffan146@gmail.com>
2025-09-06 12:21:03 +0530

    AddToCartPageTest added

---
+++ src/test/java/com/myStore/testCases/AddToCartPageTest.java
@@ -0,0 +1,41 @@
+package com.myStore.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.myStore.base.BaseClass;
+import com.myStore.pageObjects.AddToCartPage;
+import com.myStore.pageObjects.IndexPage;
+import com.myStore.pageObjects.SearchResultPage;
+
+public class AddToCartPageTest extends BaseClass {
+
+	IndexPage indexPage;
+	SearchResultPage searchResultPage;
+	AddToCartPage addToCartPage;
+
+	@BeforeMethod
+	public void setUp() {
+		launchApp();
+	}
+
+	@Test
+	public void addToCartTest() throws Throwable {
+		indexPage = new IndexPage();
+		searchResultPage = indexPage.searchProduct("t-shirt");
+		addToCartPage = searchResultPage.clickOnProduct();
+		addToCartPage.enterQuantity("2");
+		addToCartPage.selectSize("M");
+		addToCartPage.clickOnAddToCart();
+		boolean result = addToCartPage.validateAddToCart();
+		Assert.assertTrue(result);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
