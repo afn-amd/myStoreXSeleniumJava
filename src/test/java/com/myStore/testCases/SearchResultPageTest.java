54d7ff01cef702ec9e111d877330a8672cd89c24
afn-amd <ahmedaffan146@gmail.com>
2025-09-06 11:54:29 +0530

    SearchresultPageTest added

---
+++ src/test/java/com/myStore/testCases/SearchResultPageTest.java
@@ -0,0 +1,35 @@
+package com.myStore.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.myStore.base.BaseClass;
+import com.myStore.pageObjects.IndexPage;
+import com.myStore.pageObjects.SearchResultPage;
+
+public class SearchResultPageTest extends BaseClass {
+
+	IndexPage indexPage;
+	SearchResultPage searchResultPage;
+
+	@BeforeMethod
+	public void setUp() {
+		launchApp();
+	}
+
+	@Test
+	public void productAvailabilityTest() throws Throwable {
+		indexPage = new IndexPage();
+		searchResultPage = indexPage.searchProduct("t-shirt");
+		boolean result = searchResultPage.isProductAvailable();
+		Assert.assertTrue(result);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
